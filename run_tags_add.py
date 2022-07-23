e024ad84c38abbf18f702d9206ca2bbf6cd9930b
多啦b梦 <1715109585@qq.com>
2022-07-24 01:38:51 +0800

    ok

--- run_tags_add.py
+++ run_tags_add.py
@@ -12,6 +12,29 @@ def 查看系统所有环境变量():
         ic(name, value)
 # 查看系统所有环境变量()
 
+def 版本号格式加一(版本号):
+    版本号 = 版本号.split('.')
+    版本号[-1] = str(int(版本号[-1]) + 1)
+    版本号 = '.'.join(版本号)
+    return 版本号
+def 版本号从大小写排序(tags):
+    # 删除非数字的版本号
+    tags = [tag for tag in tags if tag.replace('.', '').isdigit()]
+    tags_dict = []
+    for tag in tags:
+        # 获取数值
+        tag_value = int("".join(tag.split('.')))
+        tags_dict.append({
+            "tag": tag,
+            'tagint': tag_value
+        })
+    tags_dict.sort(key=lambda student: student['tagint'])
+    tags_dict.reverse()
+    # 重新组装
+    tags = []
+    for tag in tags_dict:
+        tags.append(tag['tag'])
+    return tags
 
 def main():
     GITHUB_REPOSITORY = os.environ.get('GITHUB_REPOSITORY')
@@ -25,6 +48,25 @@ def main():
         新版本号 = "0.0.1"
         repo.create_git_ref(f"refs/tags/{新版本号}", sha)
         return 新版本号
+
+    # 版本号对比
+    tags = []
+    k = 0
+    for tag in repo.get_tags():
+        print(tag.name)
+        tags.append(tag.name)
+        k += 1
+        if k == 5:
+            break  # 取前5个标签
+    print("raw tags", tags)
+    # 版本号排序
+    tags = 版本号从大小写排序(tags)
+    # print("版本号排序:", tags)
+    新版本号 = 版本号格式加一(tags[0])
+    # print("新版本号:", 新版本号)
+    print("new tags", 新版本号)
+    sha = repo.get_commits()[0].sha
+    repo.create_git_ref(f"refs/tags/{新版本号}", sha)
 
 
 if __name__ == "__main__":
