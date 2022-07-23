baf751e2846c01f90df081d75f0d0b95a269a309
多啦b梦 <1715109585@qq.com>
2022-07-24 00:39:52 +0800

    ok

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
+# -*- coding: utf-8 -*-
+from icecream import ic
 # 打印系统所有的环境变量
 import os
-print(os.environ)
+for item in os.environ:
+    变量名 = item
+    变量值 = os.environ[item]
+    ic(变量名, 变量值)
+
 exit()
