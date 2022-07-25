71e6dc2d838e459fd8c3c1ca6fc8f28a70393b7a
多啦b梦 <1715109585@qq.com>
2022-07-26 07:24:54 +0800

    123

--- actions_读入环境变量和写出环境变量.py
+++ actions_读入环境变量和写出环境变量.py
@@ -5,28 +5,38 @@ from icecream import ic
 环境变量文件路径 = os.environ["GITHUB_ENV"]
 ic(环境变量文件路径)
 
+
 def 查看系统所有环境变量():
     for item in os.environ:
         name = item
         value = os.environ[item]
         ic(name, value)
 
-def 设置环境变量(变量名,变量值):
+
+def 设置环境变量(变量名, 变量值):
     with open(环境变量文件路径, 'a') as f:
         f.write(f"\n{变量名}={变量值}")
 
-def 读入文件变量文件(变量名,变量值):
+
+def 读入文件变量文件(变量名, 变量值):
     with open(环境变量文件路径, 'r') as f:
         读入文件变量文件 = f.read()
     ic(读入文件变量文件)
 
 
-设置环境变量("aaa","我是python设置的环境变量")
-设置环境变量("myval","重新设置的myval")
+def 设置输出变量(变量名, 变量值):
+    fprint(f"::set-output name={变量名}::{变量值}")
+
+    with open(环境变量文件路径, 'a') as f:
+        f.write(f"\n{变量名}={变量值}")
+
+
+设置环境变量("aaa", "我是python设置的环境变量")
+设置环境变量("myval", "重新设置的myval")
 
 查看系统所有环境变量()
 
-print("::set-output name=test::牛逼啊")
-print("::set-output name=test2::test厉害啊")
+设置输出变量("test", "牛逼啊")
+设置输出变量("test2", "test厉害啊")
 
 exit(0)
