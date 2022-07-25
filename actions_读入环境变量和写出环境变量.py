919b634542355d4b567960cba8ae30b3be04ba26
多啦b梦 <1715109585@qq.com>
2022-07-26 06:42:39 +0800

    ok

--- actions_读入环境变量和写出环境变量.py
+++ actions_读入环境变量和写出环境变量.py
@@ -11,4 +11,14 @@ def 查看系统所有环境变量():
 
 查看系统所有环境变量()
 
+环境变量文件路径 = os.environ["GITHUB_ENV"]
+with open(环境变量文件路径, 'r') as f:
+    文本内容 = f.read()
+print(文本内容)
+
+# 最后一行追加 123
+with open(环境变量文件路径, 'a') as f:
+    f.write(f"aaa=123")
+
+
 exit(0)
