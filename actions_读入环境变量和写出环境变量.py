056934271601618291d759a04adf7f996650c706
多啦b梦 <1715109585@qq.com>
2022-07-26 06:56:37 +0800

    ok

--- actions_读入环境变量和写出环境变量.py
+++ actions_读入环境变量和写出环境变量.py
@@ -2,26 +2,26 @@
 import os
 from icecream import ic
 
+环境变量文件路径 = os.environ["GITHUB_ENV"]
+ic(环境变量文件路径)
+
 def 查看系统所有环境变量():
     for item in os.environ:
         name = item
         value = os.environ[item]
         ic(name, value)
 
+def 设置环境变量(变量名,变量值):
+    with open(环境变量文件路径, 'a') as f:
+        f.writelines(f"{变量名}={变量值}")
+
+def 读入文件变量文件(变量名,变量值):
+    with open(环境变量文件路径, 'r') as f:
+        读入文件变量文件 = f.read()
+    ic(读入文件变量文件)
+
+
+os.environ['bbb'] = 'hello' # 这个方式是不行的
+
 查看系统所有环境变量()
-
-环境变量文件路径 = os.environ["GITHUB_ENV"]
-ic(环境变量文件路径)
-
-with open(环境变量文件路径, 'r') as f:
-    文本内容 = f.read()
-ic(文本内容)
-
-# 最后一行追加 123
-with open(环境变量文件路径, 'a') as f:
-    f.write(f"aaa=123")
-
-os.environ['bbb'] = 'hello'
-
-
 exit(0)
