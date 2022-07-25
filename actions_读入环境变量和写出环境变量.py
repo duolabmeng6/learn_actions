6d24572abc2e311695e02d26a45a82398d43168f
多啦b梦 <1715109585@qq.com>
2022-07-26 06:46:04 +0800

    ok

--- actions_读入环境变量和写出环境变量.py
+++ actions_读入环境变量和写出环境变量.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 import os
+from icecream import ic
 
 def 查看系统所有环境变量():
-    from icecream import ic
-    # 打印系统所有的环境变量
     for item in os.environ:
         name = item
         value = os.environ[item]
