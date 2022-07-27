7fc9183b7daf7dd1926c7b485aaa89ac38abcbed
多啦b梦 <1715109585@qq.com>
2022-07-27 10:47:16 +0800

    ok

--- run_write_version.py
+++ run_write_version.py
@@ -9,17 +9,17 @@ def 查看系统所有环境变量():
         value = os.environ[item]
         ic(name, value)
 # 查看系统所有环境变量()
-tagName = os.environ.get('GITHUB_REF_NAME')
-print("tagName:", tagName)
+version = os.environ.get('version')
+print("version:", version)
 # 获取运行目录
 # print("run dir:", os.getcwd())
 # 获取文件目录
 文件目录 = os.path.dirname(__file__)
 print("file dir:", os.path.dirname(__file__))
 versionFilePath = os.path.join(文件目录, "version.py")
-print("edit file {versionFilePath} output: version = {tagName}")
+print("edit file {versionFilePath} output: version = {version}")
 # 覆盖写出文件 version.py 中
 with open(versionFilePath, 'w') as f:
-    f.write(f'version = "{tagName}"')
+    f.write(f'version = "{version}"')
 
 exit()
